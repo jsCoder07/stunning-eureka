4016e13180b1b12918143849fa77b7e9be4ac53e
Jeremiah Adeboye <91225691+jsCoder07@users.noreply.github.com>
2024-07-18 01:44:43 +0100

    Add files via upload

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,31 @@
-import Image from "next/image";
+import CenterTab from "@/components/CenterTab"
+import LeftTab from "@/components/LeftTab"
+import NewPost from "@/components/NewPost"
+import NewsFeed from "@/components/NewsFeed"
+import RightTab from "@/components/RightTab"
+import Stories from "@/components/Stories"
 
-export default function Home() {
+const Homepage = () => {
   return (
-    <div className=""></div>
-  );
+    <div className='flex gap-6  pt-6'>
+      {/* Left */}
+      <div className="hidden xl:block xl:w-[20%]">
+        <LeftTab type='home'/>
+      </div>
+      {/* Center */}
+      <div className="w-full lg:w-[70%] xl:w-[50%]">
+        <div className="flex flex-col gap-6">
+        <Stories/>
+        <NewPost/>
+        <NewsFeed/>
+        </div>
+      </div>
+      {/* Right */}
+      <div className="hidden xl:block lg:w-[30%] xl:w-[30%]">
+        <RightTab/>
+      </div>
+    </div>
+  )
 }
+
+export default Homepage
